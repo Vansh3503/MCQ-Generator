3e04cc3b395265424511d0304d3d14ea968bcfe1
Vansh353 <vansh.malhotra@seaflux.tech>
2024-05-22 17:35:08 +0530

    changes in secrets key

--- src/mcqgenerator/MCQGenerator.py
+++ src/mcqgenerator/MCQGenerator.py
@@ -7,11 +7,11 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
-
+import streamlit as st
 load_dotenv()
 
 
-key=os.getenv("OPENAI_API_KEY")
+key=st.secrets["OPENAI_API_KEY"]
 
 llm=ChatOpenAI(openai_api_key=key,model_name="gpt-3.5-turbo",temperature=0.5)
 
